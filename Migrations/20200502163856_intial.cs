94152eae437e5485fb35ee276194ab94336772f7
sibbte <34045901+sibbte@users.noreply.github.com>
2020-05-02 18:17:28 +0100

    update

---
+++ Migrations/20200502163856_intial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingMicroservice.Migrations
+{
+    public partial class intial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    Notes = table.Column<string>(nullable: true),
+                    Cats = table.Column<Guid>(nullable: false),
+                    Room = table.Column<Guid>(nullable: false),
+                    Customer = table.Column<Guid>(nullable: false),
+                    BookingMade = table.Column<DateTime>(nullable: false),
+                    CheckedIn = table.Column<bool>(nullable: true),
+                    CheckedOut = table.Column<bool>(nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    CatsAmount = table.Column<int>(nullable: false),
+                    UserId = table.Column<string>(nullable: true),
+                    CCTV = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bookings");
+        }
+    }
+}
